8ae0844aba781c850646b5ffc11581259ce5d272
KennedyRyan-code <kenryan49@gmail.com>
2024-05-30 17:57:39 +0300

    Profile component implemented

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import PromptCard from "./PromptCard";
 
-const Profile = () => {
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <div>Profile</div>
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span>{name} Profile</span>
+        </h1>
+      <p className="desc text-left">{desc}</p>
+      <div>
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
