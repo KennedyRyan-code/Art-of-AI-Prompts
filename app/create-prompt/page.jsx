7ac64bb5cc78667798b15d85c5e7ad26810c135a
KennedyRyan-code <kenryan49@gmail.com>
2024-05-30 17:50:12 +0300

    created a new prompt in the database api/prompt/new

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,30 +2,39 @@
 
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
-    const [submitting, setSubmitting] = useState(false);
-    const [post, setPost] = useState({
-        prompt: "",
-        tag: "",
-    });
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
 
     const createPrompt = async (e) => {
       e.preventDefault();
       setSubmitting(true);
 
-      try {
+      try { // send post request to create a new prompt in the database api/prompt/new
         const response = await fetch('/api/prompt/new', {
           method: 'POST',
           body: JSON.stringify({
-            // post data
-          })
+            prompt: post.prompt,
+            tag: post.tag,
+            userId: session?.user.id
+          }),
         });
+
+        if (response.ok) {
+          router.push('/');
+        }
       } catch (error) {
-        
+        console.error('An unexpected error happened:', error);        
       }
     };
  
